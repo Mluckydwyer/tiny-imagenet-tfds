ea1a0b85c0bcdc32c71ca08c3d05de4d0bc505c3
hassaanakramha <hassaan.akram.ha@gmail.com>
2020-11-28 10:52:53 +0500

    removed num_shards from split generator in imagenet.py

--- tiny_imagenet/_imagenet.py
+++ tiny_imagenet/_imagenet.py
@@ -109,7 +109,7 @@ class TinyImagenetDataset(tfds.core.GeneratorBasedBuilder):
         return [
             tfds.core.SplitGenerator(
                 name=tfds.Split.TRAIN,
-                num_shards=1,
+                
                 gen_kwargs=dict(label_images=train_label_images,)),
 
             tfds.core.SplitGenerator(
